0d693d63d77402afb9a59624a3795f9e69e02218
Mathieu Leplatre <contact@mathieu-leplatre.info>
2011-11-17 23:03:39 +0100

    Add links to titles (fixes #5)

--- folder-theater.py
+++ folder-theater.py
@@ -18,6 +18,7 @@ from difflib import SequenceMatcher
 import urllib
 import optparse
 from gettext import gettext as _
+from urlparse import urlparse, urljoin
 
 try:
     from imdb import IMDb, Movie
@@ -52,12 +53,13 @@ def list_titles(path, excludes=[]):
         filename = os.path.join(path, name)
         stats = os.stat(filename)
         mtime = time.localtime(stats[8])
-        if not os.path.isdir(filename):
+        if os.path.isdir(filename):
+            basename = name
+        else:
             basename, ext = os.path.splitext(name)
-            name = basename
             if ext[1:] not in MOVIE_EXT:
                 continue
-        titleslist.append((mtime, name.decode('utf-8')))
+        titleslist.append((mtime, basename.decode('utf-8'), name))
     titleslist.sort()
     titleslist.reverse()
     return titleslist
@@ -78,7 +80,7 @@ def movie_name(filepath):
     return name.strip()
 
 
-def fetch_movie(name, filename, added, allow_empty=False):
+def fetch_movie(name, basename, filename, added, allow_empty=False):
     """ Scrap IMDB and returns a imdb.Movie object """
     ia = IMDb()
     movie = ia.search_movie(name)
@@ -115,7 +117,8 @@ def fetch_movie(name, filename, added, allow_empty=False):
     movie.search = name
     movie.added = datetime.fromtimestamp(time.mktime(added))
     movie.ageweek = (datetime.now() - movie.added).days // 7
-    movie.filename = filename
+    movie.basename = basename
+    movie.filename = urllib.quote_plus(filename.decode('utf-8'))
     movie.allocine = ALLOCINE_URL % urllib.quote_plus(fulltitle.encode('utf-8'))
     return movie
 
@@ -124,21 +127,21 @@ def build_movies(titles, all=False):
     """ Return a list of imdb.Movie objects for a list of tuples with """
     chosen = []
     uniq = []
-    for (datetime, filename) in titles:
+    for (datetime, basename, filename) in titles:
         # Guess movie names from filenames
-        title = movie_name(filename)    
+        title = movie_name(basename)    
         # Uniquify titles list
         if title not in uniq:
-            m = fetch_movie(title, filename, datetime, allow_empty=all)
+            m = fetch_movie(title, basename, filename, datetime, allow_empty=all)
             if not m:
                 continue
-            logger.info(_("Found '%s' for '%s' (%s)") % (m['title'], title, filename))
+            logger.info(_("Found '%s' for '%s' (%s)") % (m['title'], title, basename))
             chosen.append(m)
             uniq.append(title)
     return chosen
 
 
-def render_page(movies, output=None):
+def render_page(movies, output=None, urlprefix=""):
     """ Render the movies list to specified output """
     env = Environment(extensions=['jinja2.ext.i18n'])
     env.install_null_translations()
@@ -148,7 +151,7 @@ def render_page(movies, output=None):
     out = sys.stdout
     if output:
         out = open(output, 'w')
-    page = template.render(movies=movies)
+    page = template.render(movies=movies, urlprefix=urlprefix)
     out.write(page.encode('ascii', 'xmlcharrefreplace'))
     out.close()
 
@@ -170,15 +173,30 @@ if __name__ == '__main__':
     parser.add_option("-l", "--limit",
                       dest="limit", type='int', default=-1,
                       help=_("Limit list of found filenames"))
+    parser.add_option("-u", "--url",
+                      dest="urlprefix", default="",
+                      help=_("Add a link to movie titles with this URL prefix"))
 
     (options, args) = parser.parse_args(sys.argv)
     if len(args) < 2:
         parser.print_help()
         parser.exit()
+    folder = args[1]
+
+    # Sanity checks on urlprefix
+    url = options.urlprefix
+    if url:
+        if not urlparse(url).scheme:
+            url += "://"
+        if not urlparse(url).path:
+            url += os.path.abspath(folder)
+        if not url.endswith("/"):
+            url += "/"
+        options.urlprefix = url
 
     # List files/folders
-    filenames = list_titles(args[1], excludes=options.exclude.split(","))
+    filenames = list_titles(folder, excludes=options.exclude.split(","))
     if options.limit > 0:
         filenames = filenames[:options.limit]
     # Convert to HTML
-    render_page(build_movies(filenames, options.all), options.output)
+    render_page(build_movies(filenames, options.all), options.output, options.urlprefix)
